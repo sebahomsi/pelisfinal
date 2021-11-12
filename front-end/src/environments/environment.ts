8cfca98f76c1285dbe72aaa3c72c50f9e16bfaf1
sebahomsi <45207797+sebahomsi@users.noreply.github.com>
2021-11-12 19:00:36 -0300

    cambios visuales

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiURL: 'https://localhost:5001/api/'
+  apiURL: 'https://localhost:44384/api/'
 };
 /*
  * For easier debugging in development mode, you can import the following file
